80ae402b60fc692fe3a036a86772dc469fa8c012
gvtrindade <gabrielsvtrindade@gmail.com>
2021-10-22 23:46:28 -0300

    Botões de cadastro funcionais

--- src/componentes/CadastroFuncionario.js
+++ src/componentes/CadastroFuncionario.js
@@ -1,17 +1,75 @@
-import Modal from "./Modal"
+import { useState } from "react"
 
 export default function CadastroFuncionario() {
+    const [displayType, setDisplayType] = useState('none')
+    let styles = {display: displayType}
+
     const handleClick = () => {
-        
-        
+        if (displayType === 'none'){
+            setDisplayType('inline');
+        } else {
+            setDisplayType('none');
+        }
     }
 
     return (
         <div>
             <button onClick={handleClick} >Cadastrar Funcionário</button>
-            <form style={{display: 'inline'}}>
-                <h3>Teste</h3>
-            </form>
+            <div style={styles} className="form" id="formFuncionario">
+        
+                <div className="conteudoForm">
+                    <form>
+
+                        <h3> Insira as informações do funcionário:</h3>
+
+                        <div>
+                            <label for="numeroMemorando">Número:</label>
+                            <input type='text' id="numeroMemorando" />
+
+                            <br />
+                            <label for="gestorSelecionado">Assinante: </label>
+                            <select id="gestorSelecionado"></select>
+
+                            <br />
+                            <label for="lugarSelecionado">Local de trabalho:</label>
+                            <select id="lugarSelecionado">
+                                <option value = "Restaurante dos Senadores">Restaurante dos Senadores</option>
+                                <option value = "Cafézinho dos Senadores">Cafézinho dos Senadores</option>
+                                <option value = "Lanchonete do Prodasen">Lanchonete do Prodasen</option>
+                                <option value = "Casa de Massas">Casa de Massas</option>
+                            </select>
+                        </div>
+
+                        <label for="nomeFuncionario">Nome:</label>
+                        <input type='text' id="nomeFuncionario" />
+
+                        <br/>
+                        <label>Sexo:</label>
+
+                        <input type="radio" id="funcMasc" name="funcionarioSexo" value="Masculino" checked />
+                        <label for="funcMasc">Masculino</label>
+
+                        <input type="radio" id="funcFem" name="funcionarioSexo" value="Feminino" />
+                        <label for="funcFem">Feminino</label>
+
+
+                        <br />
+                        <label for="idFuncionario">Identidade:</label>
+                        <input type='text' id="idFuncionario" />
+
+                        <input onClick="listarFuncionario()" type="button" id="adicionarFuncionario" value="+" />
+                        <div>
+                            <ul id="listaNomes"></ul>
+                        </div>
+
+                        <div className="botoes">
+                            <input onClick="substituirFuncionario()" type="button" id="cadastroFuncionario" value="Cadastrar" />
+                            <input onClick={handleClick} type="button" id="cancelarFuncionario" value="Cancelar"  />
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- src/componentes/CadastroGestor.js
+++ src/componentes/CadastroGestor.js
@@ -1,10 +1,59 @@
+import { useState } from "react";
+
 export default function CadastroGestor() {
+    const [displayType, setDisplayType] = useState('none')
+    let styles = {display: displayType}
+
     const handleClick = () => {
-        
+        if (displayType === 'none'){
+            setDisplayType('inline');
+        } else {
+            setDisplayType('none');
+        }
     }
    
     return (
-        <button onClick={handleClick} >Cadastrar Gestor</button>
+        <div>
+            <button onClick={handleClick} >Cadastrar Gestor</button>
+            <div style={styles} className="form" id="formGestor">
+                <div className="conteudoForm">
+                    <form>
+
+                        <strong style={{fontSize:'20px'}}>Insira informações do novo gestor:</strong>
+
+                        <div>
+                            <label for="nomeGestor">Nome:</label>
+                            <input type="text" id="nomeGestor" />
+                        </div>
+
+                        <div>
+                            <label>Sexo:</label>
+
+                            <input type="radio" id="gestorMasc" name="gestorSexo" value="Masculino" checked />
+                            <label for="gestorMasc">Masculino</label>
+
+                            <input type="radio" id="gestorFem" name="gestorSexo" value="Feminino" />
+                            <label for="gestorFem">Feminino</label>
+                        </div>
+
+                        <input type="checkbox" id="eGestordoContrato" />
+                        <label for="eGestordoContrato">Gestor do Contrato</label>
+
+                        <div>
+                            <label for="gestorSelecionado1">Gestores: </label>
+                            <select id="gestorSelecionado1"></select>
+                            <input onClick="apagarGestor()" type="button" id="deletarGestor" value="Deletar" />
+                        </div>
+
+                        <div className="botoes">
+                            <input onClick="cadastrarGestor()" type="button" id="cadastroGestor" value="Cadastrar" />
+                            <input onClick={handleClick} type="button" id="cancelarGestor" value="Cancelar" />
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     )    
 
 }
